705256280d7d49097aa09c80425e4fb3de79fb6c
Hitori Achmad <hitori.fatchan@moladin.com>
2023-11-01 23:50:50 +0700

    use env

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const izi = require("./izi/client")
 const fs = require('fs');
-
+require('dotenv').config();
 
 function convertToBase64(filename) {
     const data = fs.readFileSync(filename, "base64");
@@ -11,14 +11,14 @@ function convertToBase64(filename) {
 
 const main = async () => {
 
-    const client = new izi("uPyDlrxRNzojitVCqmse", "PZHkAgbbTphpaiFDIzZLBohEiKnHMXKvTGbfnAeG")
+    const client = new izi(process.env.API_KEY, process.env.SECRET_KEY)
     const url = "https://apihub.izi.credit/v1/officialfacecompare";
     const body = {
         img:  convertToBase64("tes.jpeg"),
 		nik:  "3275046210900007",
 		pob:  "BEKASI",
-		dob:  "20-01-1992",
-		name: "HITORI ACHMAD FATCHAN"
+		dob:  "20-01-1994",
+		name: "HITORI ACHMAD"
     }
     const data = await client.Request(url, body)
     console.log(data.data)
